d62e88847b470d19fad8c432397ad88dd22ec89b
Gabriel Pivetta Loss <gabriellossoficial@gmail.com>
2025-02-03 09:46:51 -0300

    Add files via upload

---
+++ Aula.c
@@ -0,0 +1,44 @@
+/*
+Alocação estática:
+(+) É Rápido
+(+) Simples de implementar
+(+) Geralmente usa pouca memória
+(-) Limitadoa casos específicos
+(-) Inflexível
+
+Alocação dinâmica:
+(+) Muito flexível
+(+) Uso geral
+(+) Uso otimizado de memória
+(-) Lento
+(-) Gasto elevado de memória
+(-) Implementação mais elaborada 
+*/
+
+/*
+Tarefa: Defina estrutura da fila
+Implemente inserção
+implementar remoção
+Implementar impressão
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAM_VET 10
+
+struct fila
+{
+    int vet_fila[TAM_VET], contador;
+};
+
+typedef struct fila tipo_fila;
+
+void insere_fila(tipo_fila *,)
+
+int main(ubt argc, char *argv[]) {
+tipo_fila minha_fila;
+
+
+    return 0;
+}
